a1bce9953c4c79da99e343a61b24d8f2d889a5ec
Yash Gharat <yashgharat12@gmail.com>
2020-02-05 02:41:58 -0500

    installed flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module'
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -49,7 +50,8 @@ import { ShowContactDialogComponent } from './home/show-contact-dialog/show-cont
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    FileUploadModule
+    FileUploadModule,
+    FlexLayoutModule
   ],
   providers: [RequestHelperService],
   bootstrap: [AppComponent],
